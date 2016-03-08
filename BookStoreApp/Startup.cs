511e486abf663944bac953185eb4625edd36f7ce
yasemincidem <yasemincidem@gmail.com>
2016-03-08 10:25:32 +0200

    install autofac owin and startup configuration

--- BookStoreApp/Startup.cs
+++ BookStoreApp/Startup.cs
@@ -1,6 +1,13 @@
-﻿using Microsoft.Owin;
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.DataProtection;
 using Owin;
-
+using System;
+using System.Web;
+using System.Web.Mvc;
 [assembly: OwinStartupAttribute(typeof(BookStoreApp.Startup))]
 namespace BookStoreApp
 {
@@ -8,7 +15,25 @@ namespace BookStoreApp
     {
         public void Configuration(IAppBuilder app)
         {
-            ConfigureAuth(app);
-        }
+			//AutoFac Model Configuration
+			var builder = new ContainerBuilder();
+
+			builder.RegisterModule(new Repository.AutofacModule());
+
+			// REGISTER CONTROLLERS SO DEPENDENCIES ARE CONSTRUCTOR INJECTED
+			builder.RegisterControllers(typeof(MvcApplication).Assembly);
+
+			// BUILD THE CONTAINER
+			var container = builder.Build();
+
+			// REPLACE THE MVC DEPENDENCY RESOLVER WITH AUTOFAC
+			DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+
+			// REGISTER WITH OWIN
+			app.UseAutofacMiddleware(container);
+			app.UseAutofacMvc();
+
+			ConfigureAuth(app);
+		}
     }
 }
