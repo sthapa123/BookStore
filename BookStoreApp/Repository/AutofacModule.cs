a8ae6e500292cc5d86c5adaffb5e118302d47862
yasemincidem <yasemincidem@gmail.com>
2016-03-08 10:21:54 +0200

    install autofac and autofacModule.cs added

---
+++ BookStoreApp/Repository/AutofacModule.cs
@@ -0,0 +1,22 @@
+﻿using Autofac;
+using BookStoreApp.Repository.Implementation;
+using BookStoreApp.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStoreApp.Repository
+{
+	public class AutofacModule:Module
+	{
+		protected override void Load(ContainerBuilder builder)
+		{
+
+			builder.Register(c => new EfRepository())
+				.As<IRepository>()
+				.InstancePerLifetimeScope();
+		}
+	}
+}
