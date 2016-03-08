9cc2c17818f12bcee31c03bf267341a5cc5f8e84
yasemincidem <yasemincidem@gmail.com>
2016-03-08 10:47:01 +0200

    Dbcontext added and database created

--- BookStoreApp/Repository/Models/BookStoreContext.cs
+++ BookStoreApp/Repository/Models/BookStoreContext.cs
@@ -9,17 +9,7 @@ namespace BookStoreApp.Repository.Models
 		public BookStoreContext()
 			: base("name=BookStoreContext")
 		{
-			//lazy loading enabled is true;EF is default lazyloading true
-			//this.Configuration.LazyLoadingEnabled = true;
-			//There are four different database initialization strategies:
-			/*CreateDatabaseIfNotExists*/
-			//Database.SetInitializer<BookStoreEntities>(new CreateDatabaseIfNotExists<BookStoreEntities>());
-			/*DropCreateDatabaseIfModelChanges*/
-			//Database.SetInitializer<BookStoreEntities>(new DropCreateDatabaseIfModelChanges<BookStoreEntities>());
-			/*DropCreateDatabaseAlways*/
-			//Database.SetInitializer<BookStoreEntities>(new DropCreateDatabaseAlways<BookStoreEntities>());
-			/*Custom DB Initializer*/
-			//Database.SetInitializer<BookStoreEntities>(new BookStoreInitializer());
+
 		}
 		public DbSet<Books> Book { get; set; }
 		public DbSet<Orders> Order { get; set; }
